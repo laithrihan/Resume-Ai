e00a4fc4a7721b1e46b4027ee3a4cf02638194e2
LAITH-RIHAN <135272940+LAITH-RIHAN@users.noreply.github.com>
2026-01-09 20:16:52 +0300

    Home page and resume cards UI

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,12 +1,32 @@
+import { Navbar, ResumeCard } from "~/components/index";
 import type { Route } from "./+types/home";
+import { resumes } from "~/constants/constants";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resume AI" },
+    { name: "description", content: "Welcome to Resume AI!" },
   ];
 }
 
 export default function Home() {
-  return ;
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover bg-center bg-no-repeat">
+      <Navbar />
+      <section className="main-section">
+        <div className="page-heading py-16">
+          <h1>Track Your Applications & Resume Ratings</h1>
+          <h2>
+            Get instant feedback on your resume and track your job applications.
+          </h2>
+        </div>
+
+        <section className="resumes-section">
+          {resumes.map((resume) => (
+            <ResumeCard key={resume.id} resume={resume} />
+          ))}
+        </section>
+      </section>
+    </main>
+  );
 }
