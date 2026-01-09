1680582532f0ae8c77fda1e12b86109a76073e2e
LAITH-RIHAN <135272940+LAITH-RIHAN@users.noreply.github.com>
2026-01-10 01:03:27 +0300

    implement auth by puter

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,9 @@
 import { Navbar, ResumeCard } from "~/components/index";
 import type { Route } from "./+types/home";
 import { resumes } from "~/constants/constants";
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
+import { usePuterStore } from "~/lib/puter";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -10,11 +13,18 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+  const { auth, kv } = usePuterStore();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if(!auth.isAuthenticated) navigate('/auth?next=/');
+  }, [auth.isAuthenticated])
+
   return (
     <main className="bg-[url('/images/bg-main.svg')] bg-cover bg-center bg-no-repeat">
       <Navbar />
       <section className="main-section">
-        <div className="page-heading py-16">
+        <div className="page-heading py-">
           <h1>Track Your Applications & Resume Ratings</h1>
           <h2>
             Get instant feedback on your resume and track your job applications.
